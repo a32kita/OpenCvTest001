f80dfc084574090096061c71749747008ddc3682
a32kita <a32kita@yahoo.co.jp>
2018-07-05 23:35:08 +0900

    first commit.

---
+++ OpenCvTest001/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+
+namespace OpenCvTest001
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        // 非公開フィールド
+
+
+        // バインディングプロパティ
+
+
+        // コンストラクタ
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+
+        // 
+    }
+}
